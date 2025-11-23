ae55edceadd6d2eb9050e10293707cb9640a10e0
Uday Singh Rana <udaysr2005@gmail.com>
2025-11-24 00:06:23 +0530

    /

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,7 @@ const port = process.env.PORT;
 
 app.use(express.json());
 app.use(cors({
-    origin: "https://second-brain-frontend-beryl.vercel.app/",
+    origin: "https://second-brain-frontend-beryl.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
@@ -18,4 +18,5 @@ app.use("/api/v1/content", contentRouter);
 
 app.listen(port, () => {
     console.log(`App listening on port : ${port}`);
+
 });
