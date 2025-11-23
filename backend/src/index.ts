884973104527167e1904aedca41833f6c2c0f7bf
Uday Singh Rana <udaysr2005@gmail.com>
2025-11-24 00:15:59 +0530

    xox

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,7 @@ const port = process.env.PORT;
 
 app.use(express.json());
 app.use(cors({
-    origin: "*",
+    origin: "https://second-brain-frontend-beryl.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
